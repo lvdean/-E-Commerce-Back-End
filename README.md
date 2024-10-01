8a4d88219598cb65dbab948ccec679ee53c593e7
LAUREN <lv.dean75@gmail.com>
2024-09-30 22:44:44 -0400

    completed models files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # E-Commerce-Back-End
+This challenge was to build the back end for an e-commerce site. This was achieved by taking a working Express.js API and configure it to use Sequelize to interact with a PostgreSQL database.
--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,15 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
@@ -17,4 +25,20 @@ Category.init(
   }
 );
 
+// Export the model
 module.exports = Category;
+
+// Sample route to get all categories
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
